d84b90ee3af5951e87fdcee5f6bb9f488f70b461
athiranair2000 <athiranair2000@athinair@gmail.com>
2019-06-13 01:21:02 +0530

    Updated reminder app

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -8,8 +8,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.app.AlertDialog;
+
 import android.widget.EditText;
-import android.widget.Toast;
+
 
 import com.example.todo.R;
 
