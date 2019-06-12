1e5e485385e8b7a73fafe3143e1834b9cfc84ad9
athiranair2000 <athiranair2000@athinair@gmail.com>
2019-06-12 23:13:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,52 @@
+package example.reminder.app;
+
+import android.app.ActionBar;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.app.AlertDialog;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.todo.R;
+
+public class MainActivity extends AppCompatActivity {
+    Button Reminder;
+    AlertDialog.Builder builder;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Reminder = (Button) findViewById(R.id.reminder_btn);
+        builder = new AlertDialog.Builder(this);
+        Reminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText taskEditText = new EditText;
+
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                Log.d(TAG, "Task to add: " + task);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                ActionBar alert = null;
+                alert.show();
+            }
+        });
+    }
+}
+
