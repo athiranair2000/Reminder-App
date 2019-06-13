ea681845926be24e49bd8471730d87eb947c71fc
athiranair2000 <athiranair2000@athinair@gmail.com>
2019-06-13 23:59:48 +0530

    Updated above files

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -5,11 +5,14 @@ import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.app.AlertDialog;
 
 import android.widget.EditText;
+import android.widget.ListView;
 
 
 import com.example.todo.R;
@@ -18,11 +21,17 @@ public class MainActivity extends AppCompatActivity {
     Button Reminder;
     AlertDialog.Builder builder;
     private static final String TAG = "MainActivity";
+    private ListView mTaskListView;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTaskListView = (ListView) findViewById(R.id.list_todo);
+
+        updateUI();
+
 
         Reminder = (Button) findViewById(R.id.reminder_btn);
         builder = new AlertDialog.Builder(this);
@@ -30,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 final EditText taskEditText = new EditText;
+
+                    switch (item.getItemId()) {
+                        case R.id.action_add_task:
 
                 AlertDialog dialog = new AlertDialog.Builder(this)
                         .setTitle("Add a new task")
@@ -49,5 +61,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+        if (mAdapter == null) {
+            mAdapter = new ArrayAdapter<>(this,
+                    R.layout.item_todo,
+                    R.id.task_title,
+                    taskList);
+            mTaskListView.setAdapter(mAdapter);
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+}
 }
 
