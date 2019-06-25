ffd5dfb2fb07826c1696de26c974dfc076de5cfe
athiranair2000 <athiranair2000@athinair@gmail.com>
2019-06-25 18:54:57 +0530

    Following files are updated

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,11 +1,10 @@
-package example.reminder.app;
+package com.example.todo;
 
+import android.database.Cursor;
+import android.widget.ListView;
 import android.app.ActionBar;
 import android.content.DialogInterface;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,119 +12,97 @@ import android.widget.Button;
 import android.app.AlertDialog;
 
 import android.widget.EditText;
-import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 
-import com.example.todo.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class MainActivity extends AppCompatActivity {
+public class Activities<AppCompatActivity, mAdapter> extends AppCompatActivity {
     Button Reminder;
-    AlertDialog.Builder builder;
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
     private static final String TAG = "MainActivity";
     private ListView mTaskListView;
     private ArrayAdapter<String> mAdapter;
 
-    public MainActivity(OnFailureListener error_adding_document) {
-    }
 
-    @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTaskListView = (ListView) findViewById(R.id.list_todo);
-
-        updateUI();
-
+        mTaskListView = (ListView)findViewById(R.id.list_todo);
 
         Reminder = (Button) findViewById(R.id.reminder_btn);
-        builder = new AlertDialog.Builder(this);
         Reminder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final EditText taskEditText = new EditText;
 
-                    switch (item.getItemId()) {
-                        case R.id.action_add_task:
+                switch (item.getItemId()) {
+                    case R.id.action_add_task:
 
-                AlertDialog dialog = new AlertDialog.Builder(this)
-                        .setTitle("Add a new task")
-                        .setMessage("What do you want to do next?")
-                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                        AlertDialog dialog = new AlertDialog.Builder(this)
+                                .setTitle("Add a new task")
+                                .setMessage("What do you want to do next?")
+                                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
 
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                private static final String TAG = ;
-                                // Access a Cloud Firestore instance from your Activity
-                                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                                Map<String, Object> user = new HashMap<>();
-                                user.put();
-                                user.put();
-                                user.put();
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        final String TAG ="MainActivity";
+                                        // Access a Cloud Firestore instance from your Activity
+                                        DatabaseReference mDatabase;
+// ...
+                                        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-// Add a new document with a generated ID
-                                db.collection("users")
-                                        .add(user)
-                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                            @Override
-                                            public void onSuccess(DocumentReference documentReference) {
-                                                Log.d(TAG, "Document added with ID: " + documentReference.getId());
-                                            }
-                                        });
 
-                                new MainActivity(new OnFailureListener() {
-                                    public void onFailure(Exception e) {
-                                        Log.w(TAG, "Error adding document", e);
                                     }
-                                });
-                                db.collection("users")
-                                        .get()
-                                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                                if (task.isSuccessful()) {
-                                                    for (QueryDocumentSnapshot document : task.getResult()) {
-                                                        Log.d(TAG, document.getId() + " => " + document.getData());
-                                                    }
-                                                } else {
-                                                    Log.w(TAG, "Error getting documents.", task.getException());
-                                                }
-                                            }
-                                        });
+                                })
+                                .setNegativeButton("Cancel", null)
+                                .create();
+                        ActionBar alert = null;
+                        alert.show();
+                }
+            }});
+        private void updateUI() {
+            ArrayList<String> taskList = new ArrayList<>();
 
-                            }
-                        })
-                        .setNegativeButton("Cancel", null)
-                        .create();
-                ActionBar alert = null;
-                alert.show();
+            Cursor cursor =
+                    new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                    null, null, null, null, null);
+            while (cursor.moveToNext()) {
+                int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+                taskList.add(cursor.getString(idx));
             }
-        });
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-        if (mAdapter == null) {
-            mAdapter = new ArrayAdapter<>(this,
-                    R.layout.item_todo,
-                    R.id.task_title,
-                    taskList);
-            mTaskListView.setAdapter(mAdapter);
-        } else {
-            mAdapter.clear();
-            mAdapter.addAll(taskList);
-            mAdapter.notifyDataSetChanged();
-        }
 
-}
-}
+            if (mAdapter == null) {
+                mAdapter = new ArrayAdapter<>(this,
+                        R.layout.item_todo,
+                        R.id.task_title,
+                        taskList);
+                mTaskListView.setAdapter(mAdapter);
+            } else {
+                mAdapter.clear();
+                mAdapter.addAll(taskList);
+                mAdapter.notifyDataSetChanged();
+            }
+
+            cursor.close();
+            db.close();
+        }
+        }
+        
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
